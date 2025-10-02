633754bfe5430cec95bc7858503570478329994b
DenverW03 <denverwhiteley@hotmail.com>
2025-10-02 23:23:42 +0100

    continuous polling

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn main() {
     // ControlFlow::Wait pauses the event loop if no events are available to process.
     // This is ideal for non-game applications that only update in response to user
     // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(ControlFlow::Wait);
+    //event_loop.set_control_flow(ControlFlow::Wait);
 
     let mut app = App::default();
     event_loop.run_app(&mut app).unwrap()
